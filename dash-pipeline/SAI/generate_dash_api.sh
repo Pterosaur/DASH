e556a63c55f77899c9ca958f134d622a9a62104c
Ze Gan <ganze718@gmail.com>
2023-01-10 13:34:19 +0000

    Add libsai-dash makefile
    Signed-off-by: Ze Gan <ganze718@gmail.com>

--- dash-pipeline/SAI/generate_dash_api.sh
+++ dash-pipeline/SAI/generate_dash_api.sh
@@ -3,3 +3,6 @@
     /bmv2/dash_pipeline.bmv2/dash_pipeline_p4rt.json \
     --ignore-tables=appliance,eni_meter,slb_decap \
     dash
+cp /bmv2/dash_pipeline.bmv2/dash_pipeline_p4rt.json ./lib
+cp /bmv2/dash_pipeline.bmv2/dash_pipeline_p4rt.txt ./lib
+cp -r debian/ ./lib
